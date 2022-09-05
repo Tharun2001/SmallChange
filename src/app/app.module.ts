a91cfe673de3a3e1b9ba36a362f27be12b2c84d8
Aryan566 <aryansinha566@gmail.com>
2022-09-05 10:42:43 +0530

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LinkComponent } from './atoms/link/link.component';
+import { ButtonComponent } from './atoms/button/button.component';
+import { TextBoxStandardComponent } from './atoms/text-box-standard/text-box-standard.component';
+import { TextBoxPasswordComponent } from './atoms/text-box-password/text-box-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkComponent,
+    ButtonComponent,
+    TextBoxStandardComponent,
+    TextBoxPasswordComponent
   ],
   imports: [
     BrowserModule
