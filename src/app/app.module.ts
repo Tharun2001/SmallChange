9297bef79e780d02d7dbb1a3c50d2951799b165d
sandhyarameshiyer <sandhya.ramesh0606@gmail.com>
2022-09-05 11:33:49 +0530

    Header Logo completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderLogoComponent } from './header-logo/header-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderLogoComponent
   ],
   imports: [
     BrowserModule
