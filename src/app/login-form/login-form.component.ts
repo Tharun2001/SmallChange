49dc128e451c5465542b20616f2b7121a7a0ad41
Aryan566 <aryansinha566@gmail.com>
2022-09-05 12:47:49 +0530

    validation added

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
@@ -10,26 +9,50 @@ export class LoginFormComponent implements OnInit {
   username: string = "";
   password: string = "";
   forbiddenChars = new RegExp('\^[a-z0-9_-]*$');
-  showErrorMessage = false;
-  errorMessage = "Error";
 
 
   constructor() { }
 
   ngOnInit(): void {
-    const usernameFeild = document.querySelector('#password');
-    usernameFeild!.addEventListener('change', (e) => {
-      
+    var usernameFeild = document.getElementById('username')?.addEventListener('change', (e) => {
+      if (this.username.length >= 3 && this.username.length <= 18) {
+        if (this.forbiddenChars.test(this.username)) {
+          document.getElementById('usernameError')!.style.display = 'none';
+          document.getElementById('username')!.style.border = 'solid 1px black'
+        }
+        else {
+          document.getElementById('usernameError')!.style.display = 'block';
+          document.getElementById('username')!.style.border = 'solid 3px #DC1616'
+        }
+      }
+      else {
+        document.getElementById('usernameError')!.style.display = 'block';
+        document.getElementById('username')!.style.border = 'solid 3px #DC1616'
+      }
     })
+
+    var passwordFeild = document.getElementById('password')?.addEventListener('change', (e) => {
+      if (this.password.length >= 6 && this.password.length <= 24) {
+        if (this.forbiddenChars.test(this.password)) {
+          document.getElementById('passwordError')!.style.display = 'none';
+          document.getElementById('password')!.style.border = 'solid 1px black'
+        }
+        else {
+          document.getElementById('passwordError')!.style.display = 'block';
+          document.getElementById('password')!.style.border = 'solid 3px #DC1616'
+        }
+      }
+      else {
+        document.getElementById('passwordError')!.style.display = 'block';
+        document.getElementById('password')!.style.border = 'solid 3px #DC1616'
+      }
+    })
+
   }
 
   login() {
-    if (this.forbiddenChars.test(this.username)) {
-
-    }
-    else {
-      this.showErrorMessage = true;
-      this.errorMessage ="Invalid Username - Must contain between 3 and 18 letters, numbers, underscore or hyphens."
+    if(document.getElementById('passwordError')?.style.display == 'none' && document.getElementById('usernameError')?.style.display == 'none') {
+      console.log("Logged In");
     }
   }
 
