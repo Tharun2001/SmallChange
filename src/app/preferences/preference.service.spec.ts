839dec227d812d6c02d2cd859cf495f8b78f735b
Aryan566 <aryansinha566@gmail.com>
2022-11-10 10:30:05 +0530

    preference test

--- src/app/preferences/preference.service.spec.ts
+++ src/app/preferences/preference.service.spec.ts
@@ -1,19 +1,43 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
+import { Preference } from '../models/preference';
 
 import { PreferenceService } from './preference.service';
 
 describe('PreferenceService', () => {
-  let service: PreferenceService;
+  let mockPreferences : Preference=
+    {"username": 'Test1', "risk": 5, "purpose": "Testing", "incomeCategory": "150,000+", "lengthOfInvestment": "15+ years"};
+  let httpTestingController: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule]
     });
-    service = TestBed.inject(PreferenceService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should return preference', inject([PreferenceService],
+    fakeAsync((service: PreferenceService) =>{
+      let preference: Preference;
+      service.getPreference().subscribe();
+      const req = httpTestingController.expectOne('http://localhost:8080/api/preference');
+      expect(req.request.method).toEqual('POST');
+      req.flush(mockPreferences);
+      httpTestingController.verify();
+      tick();
+    })
+  ));
+
+  it('should set preference', inject([PreferenceService],
+    fakeAsync((service: PreferenceService) =>{
+      let cars: Preference;
+      service.setPreference(mockPreferences).subscribe(data => {
+        console.log(data);
+      });
+      const req = httpTestingController.expectOne('http://localhost:8080/api/preference');
+      expect(req.request.method).toEqual('POST');
+      httpTestingController.verify();
+      tick();
+    })
+  ));
 });
